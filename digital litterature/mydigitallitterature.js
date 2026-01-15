74b4b94cee4625bc171298047a1b3d34ec800cec
4Doigt <djenaneq@gmail.com>
2026-01-15 13:19:48 +0100

    Fetch poem words from API and refactor logic
    Refactor code to fetch words from an API instead of using a predefined array. Added functions to load and process poem data.

--- digital litterature/mydigitallitterature.js
+++ digital litterature/mydigitallitterature.js
@@ -1,11 +1,4 @@
-let mots = [                          // Tableau des mots exploitables pour le poème, plus on descend en profondeur dans le tableau plus les mots sont "poétiques"
-  ["racine", "terre", "origine"],
-  ["souffle", "ligne", "élan"],
-  ["mémoire", "fracture", "écho"],
-  ["vertige", "brume", "silence"],
-  ["absence", "invisible", "rêve"]
-];
-
+let mots = []; // Tableau vide des mots exploitables pour le poème qu'on remplit avec un appel d'API
 let poeme = []; // Tableau vide du poeme qu'on remplit grâce à genererFractale(profondeur)
 
 function setup() {
@@ -14,8 +7,31 @@ function setup() {
   textSize(16); // Taille du texte
   noLoop(); // Ne pas relancer draw() après le premier passage
 
-  // Appel Initial pour commencer le poème
-  genererFractale(0);
+  chargerPoeme();// Initialisation du poeme
+}
+
+function chargerPoeme() { // Pour récupérer des mots via une API
+  fetch("https://poetrydb.org/random") 
+    .then(res => res.json())
+    .then(data => {
+      let texte = data[0].lines.join(" "); // On concatène toutes les lignes du poème récupéré en une seule texte
+      construireMots(texte); // Récupérer les mots rangés dans leur niveau de fractal
+      genererFractale(0); // Initialisation du poème
+      redraw(); // NoLoop() empêche draw() de se lancer automatiquement donc on le force à se relancer une seule fois
+    })
+    .catch(err => console.error(err)); // En cas d'erreur
+}
+
+function construireMots(texte) { // Pour récuperer les mots séparées et crée les niveaux fractals
+  let tousLesMots = texte
+    .toLowerCase() // Transforme tout le texte en minuscule
+    .replace(/[^\w\s]/g, "") // Supprime tous les caractères qui ne sont pas lettres,chiffres ou espaces
+    .split(/\s+/); // Découpe le texte en tableau contenant tous les mots du texte
+
+  // On crée 5 niveaux fractals dans lesquels on range les mots
+  for (let i = 0; i < 5; i++) {
+    mots[i] = tousLesMots.filter(m => m.length === i + 4); // On classe les mots par longueur pour les placer dans les niveaux de fractals
+  }
 }
 
 function draw() {
@@ -36,7 +52,7 @@ function draw() {
 }
 
 function genererFractale(profondeur) { // On utilise une fractale pour générer le poème ( fct récursive)
-  if (profondeur > 4) return; // Condition d'arrêt
+  if (profondeur > 4 || mots[profondeur].length === 0) return; // Condition d'arrêt
 
   let mot = random(mots[profondeur]); // On prend un mot au hasard dans la bonne profondeur
   poeme.push(mot); // On l'ajoute à poème
